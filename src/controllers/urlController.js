8d63230363b6670adf3105aab8e2a8d01006ba93
barbararech <rech.brb@gmail.com>
2022-08-02 15:28:32 -0300

    feat: implement open url endpoint

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -42,3 +42,30 @@ export async function getURL(req, res) {
     return res.status(400).send(error);
   }
 }
+
+export async function openURL(req, res) {
+  const { shortUrl } = req.params;
+
+  try {
+    const { rows: url } = await connection.query(
+      `SELECT * FROM urls WHERE "shortUrl" = $1;`,
+      [shortUrl]
+    );
+
+    if (url.length === 0) {
+      return res.sendStatus(404);
+    }
+    console.log(shortUrl);
+    const increment = url[0].visitCount + 1;
+
+    await connection.query(
+      `UPDATE urls
+            SET "visitCount"=$1`,
+      [increment]
+    );
+
+    return res.redirect(url[0].url);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addURL, getURL } from "../controllers/urlController.js";
+import { addURL, getURL, openURL } from "../controllers/urlController.js";
 import { tokenValidationMiddleware } from "../middlewares/tokenValidationMiddleware.js";
 import { ValidateURL } from "../middlewares/urlSchemaValidationMiddleware.js";
 
@@ -8,5 +8,7 @@ const router = Router();
 router.post("/urls/shorten", tokenValidationMiddleware, ValidateURL, addURL);
 
 router.get("/urls/:id", getURL);
+
+router.get("/urls/open/:shortUrl", openURL);
 
 export default router;
