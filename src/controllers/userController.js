e7c1258696626772750f6b2bb10dc091a8c68812
barbararech <rech.brb@gmail.com>
2022-08-03 10:25:12 -0300

    feat: implement get ranking endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,7 +16,7 @@ export async function getUser(req, res) {
             'url', urls."url", 
             'visitCount', urls."visitCount")
           ORDER BY urls.id) 
-          FILTER (WHERE urls.id IS NOT NULL)
+        FILTER (WHERE urls.id IS NOT NULL)
         AS "shortenedUrls"
       FROM users
       LEFT JOIN urls 
@@ -32,6 +32,30 @@ export async function getUser(req, res) {
     }
 
     return res.status(200).send(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+}
+
+export async function getRanking(req, res) {
+  try {
+    const { rows: ranking } = await connection.query(
+      `
+      SELECT 
+        users.id, 
+        users.name, 
+        COUNT ("userId")::int AS "linksCount",
+        users."visitCount"
+      FROM users
+      LEFT JOIN urls 
+      ON urls."userId" = users.id
+      GROUP BY users.id
+      ORDER BY "visitCount" DESC
+      LIMIT 10
+      `
+    );
+
+    return res.status(200).send(ranking);
   } catch (error) {
     return res.status(500).send(error);
   }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUser } from "../controllers/userController.js";
+import { getUser, getRanking } from "../controllers/userController.js";
 import { tokenValidationMiddleware } from "../middlewares/tokenValidationMiddleware.js";
 
 const router = Router();
 
 router.get("/users/me", tokenValidationMiddleware, getUser);
+router.get("/ranking", getRanking);
 
 export default router;
